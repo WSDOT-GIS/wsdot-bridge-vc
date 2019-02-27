be1cae41b2df9c0a0efa5275323605dc08fca3b4
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2019-02-26 17:25:11 -0800

    initial commit :checkered_flag:

---
+++ README.md
@@ -0,0 +1,3 @@
+# Query Client for WSDOT Bridge Vertical Clearance ArcGIS Map Service
+
+Not ready for production. Work-in-progress.
---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import { request } from "@esri/arcgis-rest-request";
+
+const defaultMapServiceUrl =
+  "https://data.wsdot.wa.gov/arcgis/rest/services/Bridge/BridgeVerticalClearance/MapServer";
+
+export type Direction = "I" | "D" | "B";
+
+export interface ILaneInfo {
+  LaneNumber: number;
+  MilepostDirectionCode: string;
+  VerticalClearanceMinimum: number;
+}
+
+export interface IAdvisoryNote {
+  DirectionInd: Direction;
+  AdvisoryNote: string;
+}
+
+export interface IDocumentInfo {
+  BridgeDocumentId: number;
+  MilepostDirectionCode: Direction;
+}
+
+export interface IFeature<T> {
+  attributes: T;
+}
+
+export interface IQueryResponse<T> {
+  features: IFeature<T>[];
+}
+
+export async function getRelatedInfo<T>(
+  crossingLocationId: number,
+  tableId: number,
+  outFields: string[],
+  mapServiceUrl: string = defaultMapServiceUrl
+) {
+  const where = `CrossingLocationId = ${crossingLocationId}`;
+  const queryUrl = `${mapServiceUrl.replace(/\/?$/, "")}/${tableId}/query`;
+  const queryResponse: IQueryResponse<T> = await request(queryUrl, {
+    params: {
+      where,
+      outFields: outFields.join(",")
+    }
+  });
+  return queryResponse.features.map(f => f.attributes);
+}
+
+export async function getLaneInfo(crossingLocationId: number) {
+  return await getRelatedInfo<ILaneInfo>(crossingLocationId, 2, [
+    "LaneNumber",
+    "MilepostDirectionCode",
+    "VerticalClearanceMinimum"
+  ]);
+}
+
+export async function getAdvisoryNotes(crossingLocationId: number) {
+  return await getRelatedInfo<IAdvisoryNote>(crossingLocationId, 4, [
+    "DirectionInd",
+    "AdvisoryNote"
+  ]);
+}
+
+export async function getDocumentInfos(crossingLocationId: number) {
+  return await getRelatedInfo<IDocumentInfo>(crossingLocationId, 3, [
+    "BridgeDocumentId",
+    "MilepostDirectionCode"
+  ]);
+}
