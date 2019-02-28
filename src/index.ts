42eb22d201f386035f649bc123584312ea3bb682
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2019-02-28 09:30:37 -0800

    fix: map service url wasn't being passed to child function calls

--- src/index.ts
+++ src/index.ts
@@ -5,30 +5,52 @@ const defaultMapServiceUrl =
 
 export type Direction = "I" | "D" | "B";
 
+/**
+ * Attributes of a lane record
+ */
 export interface ILaneInfo {
   LaneNumber: number;
   MilepostDirectionCode: string;
   VerticalClearanceMinimum: number;
 }
 
+/**
+ * Attributes of an Advisory Note record
+ */
 export interface IAdvisoryNote {
   DirectionInd: Direction;
   AdvisoryNote: string;
 }
 
+/**
+ * Attributes of a Document record.
+ */
 export interface IDocumentInfo {
   BridgeDocumentId: number;
   MilepostDirectionCode: Direction;
 }
 
+/**
+ * An ArcGIS Feature
+ */
 export interface IFeature<T> {
   attributes: T;
 }
 
+/**
+ * The response of a map service layer query request.
+ */
 export interface IQueryResponse<T> {
   features: IFeature<T>[];
 }
 
+/**
+ * Queries a map service table for features related to a specific crossing location.
+ * @param crossingLocationId
+ * @param tableId
+ * @param outFields
+ * @param mapServiceUrl
+ */
 export async function getRelatedInfo<T>(
   crossingLocationId: number,
   tableId: number,
@@ -46,32 +68,52 @@ export async function getRelatedInfo<T>(
   return queryResponse.features.map(f => f.attributes);
 }
 
-export async function getLaneInfo(crossingLocationId: number) {
+/**
+ * Gets lane info for a specific crossing location.
+ * @param crossingLocationId
+ * @param mapServiceUrl
+ */
+export async function getLaneInfo(crossingLocationId: number, mapServiceUrl: string = defaultMapServiceUrl) {
   return await getRelatedInfo<ILaneInfo>(crossingLocationId, 2, [
     "LaneNumber",
     "MilepostDirectionCode",
     "VerticalClearanceMinimum"
-  ]);
+  ], mapServiceUrl);
 }
 
-export async function getAdvisoryNotes(crossingLocationId: number) {
+/**
+ * Gets advisory notes for a specific crossing location.
+ * @param crossingLocationId
+ * @param mapServiceUrl
+ */
+export async function getAdvisoryNotes(crossingLocationId: number, mapServiceUrl: string = defaultMapServiceUrl) {
   return await getRelatedInfo<IAdvisoryNote>(crossingLocationId, 4, [
     "DirectionInd",
     "AdvisoryNote"
-  ]);
+  ], mapServiceUrl);
 }
 
-export async function getDocumentInfos(crossingLocationId: number) {
+/**
+ * Gets info about documents related to a given crossing location.
+ * @param crossingLocationId
+ * @param mapServiceUrl
+ */
+export async function getDocumentInfos(crossingLocationId: number, mapServiceUrl: string = defaultMapServiceUrl) {
   return await getRelatedInfo<IDocumentInfo>(crossingLocationId, 3, [
     "BridgeDocumentId",
     "MilepostDirectionCode"
-  ]);
+  ], mapServiceUrl);
 }
 
-export async function getAllRelatedInfo(crossingLocationId: number) {
-  const laneInfoTask = getLaneInfo(crossingLocationId);
-  const advisoryNotesTask = getAdvisoryNotes(crossingLocationId);
-  const documentInfoTask = getDocumentInfos(crossingLocationId);
+/**
+ * Retrieves lane info, advisory notes, and document info for a crossing location.
+ * @param crossingLocationId
+ * @param mapServiceUrl
+ */
+export async function getAllRelatedInfo(crossingLocationId: number, mapServiceUrl: string = defaultMapServiceUrl) {
+  const laneInfoTask = getLaneInfo(crossingLocationId, mapServiceUrl);
+  const advisoryNotesTask = getAdvisoryNotes(crossingLocationId, mapServiceUrl);
+  const documentInfoTask = getDocumentInfos(crossingLocationId, mapServiceUrl);
 
   return Promise.all([laneInfoTask, advisoryNotesTask, documentInfoTask]);
 
