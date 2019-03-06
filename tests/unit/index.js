ef9980da24cc2f3b2abf47a5a7d724f5db756cbd
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2019-03-05 16:02:10 -0800

    modified package output

--- tests/unit/index.js
+++ tests/unit/index.js
@@ -1,15 +1,20 @@
-const { fetchCrossingInfo } = require("../../dist/index");
-require("isomorphic-fetch");
+const { fetchCrossingInfo } = require("../../dist/commonjs/index");
+if (typeof fetch === "undefined") {
+  require("isomorphic-fetch");
+}
 
-const { describe, it } = intern.getPlugin('interface.bdd');
-const { assert } = intern.getPlugin('chai');
+const { describe, it } = intern.getPlugin("interface.bdd");
+const { assert } = intern.getPlugin("chai");
 
-describe('bridge-vc', () => {
-    it('should work', async () => {
-        // TODO: Update test to use production service when available.
-        let crossing = await fetchCrossingInfo(5672, "http://localhost:6080/arcgis/rest/services/Bridge/BridgeVerticalClearance/MapServer");
-        assert.property(crossing, "CrossingLocation");
-        assert.property(crossing, "RelatedData");
-        return;
-    })
+describe("bridge-vc", () => {
+  it("should work", async () => {
+    // TODO: Update test to use production service when available.
+    let crossing = await fetchCrossingInfo(
+      5672,
+      "http://localhost:6080/arcgis/rest/services/Bridge/BridgeVerticalClearance/MapServer"
+    );
+    assert.property(crossing, "CrossingLocation");
+    assert.property(crossing, "RelatedData");
+    return;
+  });
 });
